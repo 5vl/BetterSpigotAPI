adc71d269a7dab2012b1fa4972b4b97429fe40ce
5vl <nederlandmax@gmail.com>
2022-11-21 00:55:55 +0100

    did some documentation

--- src/main/java/me/fivevl/betterspigot/BetterSpigot.kt
+++ src/main/java/me/fivevl/betterspigot/BetterSpigot.kt
@@ -2,8 +2,19 @@ package me.fivevl.betterspigot
 
 import org.bukkit.plugin.java.JavaPlugin
 
+/**
+ * @author 5vl
+ * @since 0.0.1
+ */
 object BetterSpigot {
+    /**
+     * @return The API users JavaPlugin instance
+     */
     lateinit var instance: JavaPlugin
+
+    /**
+     * Initializes the API
+     */
     fun BetterSpigot(plugin: JavaPlugin) {
         instance = plugin
     }
--- src/main/java/me/fivevl/betterspigot/BetterText.kt
+++ src/main/java/me/fivevl/betterspigot/BetterText.kt
@@ -9,6 +9,7 @@ import net.md_5.bungee.api.ChatColor
 object BetterText {
     /**
      * @return The color-translated string
+     * @sample String#color() - "&aGreen, &cRed and &bBlue".color()
      */
     fun String.color(): String {
         return ChatColor.translateAlternateColorCodes('&', this)
