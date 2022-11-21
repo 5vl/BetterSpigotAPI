9cc9426a6888e9773356d93140bf4314d2e9d4ba
5vl <nederlandmax@gmail.com>
2022-11-21 01:39:22 +0100

    added default yml creator

--- src/main/java/me/fivevl/betterspigot/BetterData.kt
+++ src/main/java/me/fivevl/betterspigot/BetterData.kt
@@ -1,8 +1,20 @@
 package me.fivevl.betterspigot
 
+import java.io.File
+
 /**
  * @author 5vl
  * @since 0.0.1
  */
 object BetterData {
+    /**
+     * Creates default yml file from resources folder if it doesn't exist yet
+     * @param fileName The name of the file to create
+     */
+    fun createDefaultYml(fileName: String) {
+        val dataFolder = BetterSpigot.instance.dataFolder
+        if (!dataFolder.exists()) dataFolder.mkdir()
+        val file = File(dataFolder, fileName)
+        if (!file.exists()) BetterSpigot.instance.getResource(fileName)?.copyTo(file.outputStream())
+    }
 }
